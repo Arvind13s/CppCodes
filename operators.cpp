682a3d4d87d2542909aa2b2a5430b8df8226d995
Arvind singh <111983100+Arvind13s@users.noreply.github.com>
2025-06-18 23:08:41 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Arithmatic operator
+    // int a;
+    // double b;
+    // cin>>a>>b;
+    // cout<<"sum = " << (a+b) << endl;
+    // cout<<"diffeence = "<<(a-b)<<endl;
+    // cout<<"product = "<<(a*b)<<endl;
+    // cout<<"division = "<<(a/b)<<endl;
+    // cout<<"remainder = "<<(a%b)<<endl;
+    
+    // Relational operator(always print boolean values )
+    // cout<<(3<5)<<endl; //true-->1
+    // cout<<(3>5)<<endl; //false-->0
+    // cout<<(3<=3)<<endl; //true-->1
+    // cout<<(3 == 3) <<endl; //true-->1
+    // cout<<(3!=3)<<endl; //false-->0
+    
+    //Logical Operators
+    // cout << (( 3 < 1) || (3 < 5)) << endl;//(OR) 
+    // (if multiple answer are given and in 
+    //that if any one is True then the final answer is True ,
+    //or both are True then True, if both False then false.)   
+    // cout << ((3 > 2) && (3 > 1))<<endl;//(AND)
+    //if multiple answer are given and in that if Both are True 
+    //then Final answer is True , but if one true and other is false
+    //then final answer will be false, and if both are false then also final answer is false.
+    // cout<<!(3>2)<<endl;(NOT)
+    //if the answer is True then it will make it false 
+    //using ! in that .
+    
+    //Unary Operators
+    //Increment++
+    //1.Post Increment Operators(a++)
+    //(work-->Update)
+    // int a = 10;
+    // int b = a++;
+    // cout<<b<<endl;//10
+    // cout<<a<<endl;//11
+    //2.Pre Increment Operator(++a)
+    //(Update-->Work)
+    // int a = 10;
+    // int b = ++a;
+    // cout<<b<<endl;//11
+    // cout<<a<<endl;//11
+    
+    //Decrement--
+    //1.Post Decrement Operators(a--)
+    //(work-->Update)
+    // int a = 10;
+    // int b = a--;
+    // cout<<b<<endl;//10
+    // cout<<a<<endl;//09
+    //2.Pre Decrement Operator(--a)
+    //(Update-->Work)
+    // int a = 10;
+    // int b = --a;
+    // cout<<b<<endl;//09
+    // cout<<a<<endl;//09
+    //Bitwise Operators
+    // cout<<(3&5)<<endl; //Bitwise AND 
+    // cout<<(3|5)<<endl; //Bitwise OR
+    // cout<<(3^5)<<endl; //Bitwise XOR
+    // cout<<(3<<1)<<endl; //Left Shift
+    // cout<<(3>>1)<<endl; //Right Shift
+    return 0;
+}
