aaad7b56e81e217898b79b094ade93bc2ff7d66a
Arvind singh <111983100+Arvind13s@users.noreply.github.com>
2025-06-19 20:24:06 +0530

    Add files via upload

---
+++ Conditional Statements.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //conditional statements
+    // char ch;
+    // cout<< "Enter the char: "<<endl;
+    // cin>>ch;
+    // if(ch>='a' && ch<='z') {
+    //     cout<<"Lowercase\n"<<endl;
+    // }
+    // else if(ch>='A' && ch<='Z'){
+    //     cout<<"uppercase\n"<<endl;
+    // }
+    // else {
+    //     cout<<"Error\n"<<endl;
+    // }
+    
+    // char ch;
+    // cout<< "Enter the char: "<<endl;
+    // cin>>ch;
+    // if(ch>=65 && ch<=90) {
+    //     cout<<"uppercase\n"<<endl;
+    // }
+    // else {
+    //     cout<<"lowercase\n"<<endl;
+    // }
+    
+    //ternary conditional statement
+    int n;
+    cin>>n;
+    cout<<(n>=0?"Positive":"Negative")<<endl;
+
+    return 0;
+}
